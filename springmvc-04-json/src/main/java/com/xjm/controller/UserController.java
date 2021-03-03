2d8d45d29b40849d0be40e8373d10c35586f2352
JianMin-Xie <854377742@qq.com>
2021-03-03 15:40:38 +0800

    5.FastJson

--- springmvc-04-json/src/main/java/com/xjm/controller/UserController.java
+++ springmvc-04-json/src/main/java/com/xjm/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.xjm.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xjm.pojo.User;
@@ -7,7 +9,9 @@ import com.xjm.utils.JsonUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Panda
@@ -25,9 +29,43 @@ public class UserController {
     }
 
     @RequestMapping("/j2")
-    public String json5() throws JsonProcessingException {
+    public String json2() throws JsonProcessingException {
         Date date = new Date();
         String json = JsonUtils.getJson(date);
         return json;
+    }
+    @RequestMapping("/j3")
+    public String json3() throws JsonProcessingException {
+        List<User> userList = new ArrayList<User>();
+        User user1 = new User("谢健民1号",18,"男");
+        User user2 = new User("谢健民2号",18,"男");
+        User user3 = new User("谢健民3号",18,"男");
+        User user4 = new User("谢健民4号",18,"男");
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+
+        String str = JSON.toJSONString(userList);
+
+        System.out.println("*******Java对象 转 JSON字符串*******");
+        String str1 = JSON.toJSONString(userList);
+        System.out.println("JSON.toJSONString(list)==>"+str1);
+        String str2 = JSON.toJSONString(user1);
+        System.out.println("JSON.toJSONString(user1)==>"+str2);
+
+        System.out.println("\n****** JSON字符串 转 Java对象*******");
+        User jp_user1=JSON.parseObject(str2,User.class);
+        System.out.println("JSON.parseObject(str2,User.class)==>"+jp_user1);
+
+        System.out.println("\n****** Java对象 转 JSON对象 ******");
+        JSONObject jsonObject1 = (JSONObject) JSON.toJSON(user2);
+        System.out.println("(JSONObject) JSON.toJSON(user2)==>"+jsonObject1.getString("name"));
+
+        System.out.println("\n****** JSON对象 转 Java对象 ******");
+        User to_java_user = JSON.toJavaObject(jsonObject1, User.class);
+        System.out.println("JSON.toJavaObject(jsonObject1, User.class)==>"+to_java_user);
+
+        return "hello";
     }
 }
